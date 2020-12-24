9549f0d6340dac7ea1d132ca9e0987a6ecd48b48
Luis Xavier Perez <xavierpm1221@gmail.com>
2020-12-24 01:00:11 -0600

    Animating the hero with gsap, TweenMax

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,66 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import Navbar from './Navbar';
 import Footer from './Footer';
 
+import { TweenMax, Power3 } from 'gsap';
+
 import './styles/App.css';
 
 function App() {
+    let headline = useRef(null);
+
+    useEffect(() => {
+        console.log(headline);
+        TweenMax.fromTo(
+            headline,
+            1.2,
+            { 
+                opacity: 0,
+                y: 40
+            },
+            { 
+                opacity: 1,
+                y: 0,
+                ease: Power3.easeInOut
+            }
+        )
+    }, []);
+
     return (
-    <div className="App">
-        <Navbar/>
-            <section className="home">
-                HOME
-            </section>
-            <section className="portfolio">
-                PORTFOLIO
-            </section>
-            <section className="education">
-                EDUCATION
-            </section>
-            <section className="about">
-                ABOUT
-            </section>
-            <section className="contact">
-                CONTACT
-            </section>
-        <Footer/>
-    </div>
+        <div className="App">
+                <header className="header">
+                    <Navbar/>
+                    <section className="hero">
+                        <div className="hero-headline" ref={el => {headline = el}}>
+                            <p className="headline__greeting">Hi!, my name is</p>
+                            <h1 className="headline__name">Luis Xavier Pérez</h1>
+                            <h2 className="headline__career"> Mechatronics Engineer and Software Developer</h2>
+                        </div>
+                        {/* <img className="image_model" src="https://i.imgur.com/BRIKgoX.jpg" alt="me_posing"/> */}
+                    </section>
+                </header>
+
+                <div className="slider"></div>
+
+                <section className="portfolio">
+                    PORTFOLIO
+                </section>
+                
+                <section className="education">
+                    EDUCATION
+                </section>
+                
+                <section className="about">
+                    ABOUT
+                </section>
+                
+                <section className="contact">
+                    CONTACT
+                </section>
+                
+                <Footer/>
+        </div>
     );
 }
 
